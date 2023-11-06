9c5473ab52f5114d80dfdc7c96ae01162324c913
anhsguy <148895612+anhsguy@users.noreply.github.com>
2023-11-06 15:53:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ def hour_to_minutes(hours):
     # raise TypeError('Hours must be a number')
     return 'hour should be int or float'
   return hours * 60  
+if --name--=='--main--':
+  print (hour_to_minutes(1.5))
